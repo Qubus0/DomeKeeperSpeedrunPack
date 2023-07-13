7f6c5a42e01ba349e8d18a597ee1f624fcbe5579
Qubus0 <steen.rickmer@gmx.de>
2023-07-13 16:31:20 +0200

    fix deprecations

--- TeamSpeed-SpeedrunPack/mod_main.gd
+++ TeamSpeed-SpeedrunPack/mod_main.gd
@@ -8,17 +8,17 @@ var mod_dir_path := ""
 var extensions_dir_path := ""
 
 
-func _init(modLoader = ModLoader) -> void:
-	mod_dir_path = modLoader.UNPACKED_DIR.plus_file(MOD_DIR)
+func _init() -> void:
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(MOD_DIR)
 
 	# Add extensions
-	install_script_extensions(modLoader)
+	install_script_extensions()
 
 
-func install_script_extensions(modLoader) -> void:
+func install_script_extensions() -> void:
 	# Any script extensions should go in this directory, and should follow the same directory structure as vanilla
 	extensions_dir_path = mod_dir_path.plus_file("extensions")
-	modLoader.install_script_extension(extensions_dir_path.plus_file("content/gamemode/relichunt/Relichunt.gd"))
-	modLoader.install_script_extension(extensions_dir_path.plus_file("stages/level/LevelStage.gd"))
+	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("content/gamemode/relichunt/Relichunt.gd"))
+	ModLoaderMod.install_script_extension(extensions_dir_path.plus_file("stages/level/LevelStage.gd"))
 
 
