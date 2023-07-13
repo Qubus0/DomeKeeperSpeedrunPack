88c9efa72b4373a26be6a8cfc399f85cbc06c484
Qubus0 <steen.rickmer@gmx.de>
2023-07-13 20:34:50 +0200

    add seed to pause menu

--- TeamSpeed-SpeedrunPack/extensions/stages/level/LevelStage.gd
+++ TeamSpeed-SpeedrunPack/extensions/stages/level/LevelStage.gd
@@ -1,5 +1,6 @@
 extends "res://stages/level/LevelStage.gd"
 
+var LOG_NAME := "TeamSpeed-SpeedrunPack:LevelStage"
 
 func _input(event: InputEvent) -> void:
 	var key_event := event as InputEventKey
@@ -17,4 +18,33 @@ func _input(event: InputEvent) -> void:
 		startData.loadout = GameWorld.loadoutStageConfig.duplicate()
 
 		StageManager.startStage("stages/landing/landing", [startData])
+
+
+func openPauseMenu():
+	.openPauseMenu()
+
+	var pause_menu: Node
+	for node in get_tree().get_nodes_in_group("gamepadListeners"):
+		if node.name == "PauseMenu":
+			pause_menu = node
+
+	if not pause_menu:
+		ModLoaderLog.warning("Node PauseMenu not found", LOG_NAME)
+		return
+
+	var menu_panel := pause_menu.get_node("MenuPanel")
+	if not menu_panel:
+		ModLoaderLog.warning("Node MenuPanel not found under PauseMenu", LOG_NAME)
+		return
+
+	var vbox := menu_panel.get_node("VBoxContainer")
+	if not vbox:
+		ModLoaderLog.warning("Node VBoxContainer not found under MenuPanel", LOG_NAME)
+		return
+
+	var seed_label := Label.new()
+	seed_label.text = "Seed: %s" % Level.levelSeed
+	seed_label.align = Label.ALIGN_CENTER
+	vbox.add_child(seed_label)
+
 
